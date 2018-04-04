8c69f4e4d95ff9a06fb5bc2152543fca07383406
jasonluck <jluck@meetveracity.com>
2018-04-04 14:44:26 -0400

    add slave output

--- modules/dns-cluster/outputs.tf
+++ modules/dns-cluster/outputs.tf
@@ -2,8 +2,8 @@ output "dns_master_private_ip" {
   value = "${aws_instance.master.private_ip}"
 }
 
-output "dns_master_public_ip" {
-  value = "${aws_instance.master.public_ip}"
+output "dns_slave_private_ips" {
+  value = "${join(",", aws_instance.slave.*.private_ip)}"
 }
 
 output "security_group_id" {
